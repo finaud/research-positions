9048abe74a726f69bed520bdc3331136c555b25c
Antoine Finot <antoine.finot@mail.utoronto.ca>
2019-07-04 23:28:05 +0200

    added skeleton for auth

--- views/auth/controllers.py
+++ views/auth/controllers.py
@@ -27,3 +27,33 @@ def prof_required(view):
 @auth.route('/')
 def index():
     return "auth view"
+
+
+@auth.route('/register', methods=['GET'])
+def register():
+    pass
+
+
+@auth.route('/register_student', methods=['POST'])
+def register_student():
+    pass
+
+
+@auth.route('/register_prof', methods=['POST'])
+def register_prof():
+    pass
+
+
+@auth.route('/login', methods=['GET'])
+def login():
+    pass
+
+
+@auth.route('/login_student', methods=['POST'])
+def login_student():
+    pass
+
+
+@auth.route('/login_prof', methods=['POST'])
+def login_prof():
+    pass
